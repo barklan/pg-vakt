cec1ea7269bd4938c1a1cceb2a4624ca7e987f34
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-12-11 01:20:24 +0300

    chore: rm redundant code

--- pkg/core.go
+++ pkg/core.go
@@ -1,11 +1,7 @@
 package pkg
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
 	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
@@ -25,23 +21,4 @@ func Bot(botToken string) *tb.Bot {
 
 func GetMainChat(chatID int64) *tb.Chat {
 	return &tb.Chat{ID: chatID}
-}
-
-// Direct request to telegram api. Should not be used.
-func request(botMethod string) ([]byte, error) {
-	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
-	req, _ := http.NewRequest(http.MethodGet,
-		fmt.Sprintf("https://api.telegram.org/bot%s/%s", botToken, botMethod), nil)
-	log.Println("Sending request to telegram api.")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Printf("Telegram client get failed: %s\n", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	log.Println(string(body))
-	return body, nil
 }
